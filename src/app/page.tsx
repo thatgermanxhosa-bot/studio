e543ecc11788d386290200f79ef87e07011e29d3
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-16 22:46:36 +0000

    unmute it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         src="/Pichulik_Studios_Webpage_3.mp4"
         autoPlay
         loop
-        muted
         playsInline
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover z-0"
       >
