4d86b75af6edc3e8e0417a6a577050943ad40831
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-16 22:43:36 +0000

    cant see my video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
         src="/Studios%20Webpage_3.mp4"
         autoPlay
         loop
+        muted
         playsInline
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover z-0"
       >
