30f901eceed1fd351a9735af01b8ab0a2810e974
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-16 00:42:07 +0000

    MEDIA ISNT WORKING

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         muted
         playsInline
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover z-0"
+        key="video-background" 
       >
         <source src="/Pichulik-Studios-Webpage.mp4" type="video/mp4" />
         Your browser does not support the video tag.
