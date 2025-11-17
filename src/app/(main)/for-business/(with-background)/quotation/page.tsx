b224a012454810592673c1bfb20e5f0e59c5961e
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-17 03:48:32 +0000

    Now slightly shift all the text up

--- src/app/(main)/for-business/(with-background)/quotation/page.tsx
+++ src/app/(main)/for-business/(with-background)/quotation/page.tsx
@@ -5,7 +5,7 @@ import { QuotationForm } from "./quotation-form";
 export default function QuotationPage() {
   return (
     <div className="relative z-10">
-      <section className="bg-transparent py-16 md:py-24 text-center pt-32 md:pt-48">
+      <section className="bg-transparent py-16 md:py-24 text-center pt-40 md:pt-48">
         <div className="container">
           <h1 className="text-4xl md:text-5xl font-black">Ready to elevate your brand's story?</h1>
            <p className="mt-4 text-lg max-w-2xl mx-auto text-white/80">
