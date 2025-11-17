6f284f05e72b1ed980aeba6f1dbfcd394b417e9a
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-17 06:12:04 +0000

    the header is gone now

---
+++ src/app/(locked)/layout.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function LockedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen bg-background text-foreground">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
