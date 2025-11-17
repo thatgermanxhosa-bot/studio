3b763484826b94361c5ec867742ff92f7e42bd34
AL Pichulik <thatgermanxhosa@gmail.com>
2025-11-17 03:54:54 +0000

    undo that

--- src/app/(main)/for-business/(with-background)/quotation/page.tsx
+++ src/app/(main)/for-business/(with-background)/quotation/page.tsx
@@ -4,9 +4,11 @@ import { QuotationForm } from "./quotation-form";
 export default function QuotationPage() {
   return (
     <div className="relative z-10">
-      <section className="bg-transparent py-16 md:py-24 text-center pt-32 md:pt-40">
+      <section className="bg-transparent text-center pt-24 md:pt-32">
         <div className="container">
-          <h1 className="text-4xl md:text-5xl font-black">Ready to Elevate Your Brand's Story?</h1>
+          <h1 className="text-4xl md:text-5xl font-black">
+            Ready to elevate your brand's story?
+          </h1>
            <p className="mt-4 text-lg max-w-2xl mx-auto text-white/80">
             This is where the partnership begins. Whether you have a full project brief or just an initial business goal, we're ready to discuss how we can translate your vision into high-impact creative.
           </p>
