9822120e6e1cb7fbb6d7302c4b172d0e2df44b4d
AL Pichulik <thatgermanxhosa@gmail.com>
2025-12-01 09:29:08 +0000

    image is broken

--- src/app/(main)/for-personal/(with-background)/layout.tsx
+++ src/app/(main)/for-personal/(with-background)/layout.tsx
@@ -19,21 +19,27 @@ export default function ForPersonalWithBackgroundLayout({
 
   return (
     <div className="relative min-h-screen">
-      {backgroundImage && (
-        <div className="fixed inset-0 z-0">
-          <Image
-            src={backgroundImage}
-            alt="Pichulik Studios Background"
-            fill
-            className="object-cover"
-            priority
-          />
-          <div className="absolute inset-0 bg-black/60"></div>
+      {backgroundImage ? (
+        <>
+          <div className="fixed inset-0 z-0">
+            <Image
+              src={backgroundImage}
+              alt="Pichulik Studios Background"
+              fill
+              className="object-cover"
+              priority
+            />
+            <div className="absolute inset-0 bg-black/60"></div>
+          </div>
+          <div className="relative z-20 flex flex-col min-h-screen text-white">
+            <main className="flex-1">{children}</main>
+          </div>
+        </>
+      ) : (
+        <div className="bg-background text-foreground">
+           <main className="flex-1">{children}</main>
         </div>
       )}
-      <div className="relative z-20 flex flex-col min-h-screen text-white">
-        <main className="flex-1">{children}</main>
-      </div>
     </div>
   );
 }
