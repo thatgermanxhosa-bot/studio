bed1a701d8a28b3df0a01c49feb46e36ad9cb350
AL Pichulik <thatgermanxhosa@gmail.com>
2025-12-13 09:45:52 +0000

    use the exact files in that path. dont rename anything

--- src/app/(app)/for-business/our-work/page.tsx
+++ src/app/(app)/for-business/our-work/page.tsx
@@ -1,10 +1,11 @@
 
 'use client';
 
+import React from "react";
 import Image from "next/image";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
 import { PlayCircle } from "lucide-react";
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
